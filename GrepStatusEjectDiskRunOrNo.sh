838fb00b0c542e1826cfaa85ae4f1fa2607d599d
Agung Sujiwo <agung_sujiwo@bsj.sch.id>
2025-01-22 17:03:46 +0700

    Update GrepStatusEjectDiskRunOrNo.sh

--- GrepStatusEjectDiskRunOrNo.sh
+++ GrepStatusEjectDiskRunOrNo.sh
@@ -9,7 +9,7 @@ process_info=$(ps aux | grep "[A]utoEjectDiskExternal.sh")
 if [ -n "$process_info" ]; then
     # Jika proses ditemukan, ambil kolom ke-11 sampai akhir
     script_command=$(echo "$process_info" | awk '{for (i=11; i<=NF; i++) printf $i " "; print ""}')
-    echo "AutoEjectDiskExternal.sh aktif: $script_command"
+    echo "AutoEjectDiskExternal.sh Running: $script_command"
 else
-    echo "AutoEjectDiskExternal.sh tidak aktif"
+    echo "AutoEjectDiskExternal.sh Not Running"
 fi
